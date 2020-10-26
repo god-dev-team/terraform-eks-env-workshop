774ec23a449044d6f52a8d82333ca858c96e1c17
Timur Galeev <timur_galeev@outlook.com>
2020-10-26 21:52:27 +0100

    Fix argo

--- charts/modules/argo/main.tf
+++ charts/modules/argo/main.tf
@@ -46,6 +46,29 @@ resource "helm_release" "argo-events" {
   create_namespace = true
 }
 
+resource "helm_release" "argo-events-webhook" {
+  count      = var.argo_count ? 1 : 0
+
+  repository = "https://kubernetes-charts-incubator.storage.googleapis.com"
+  chart      = "raw"
+
+  namespace = "argo-events"
+  name      = "argo-events-webhook"
+
+  values = [
+    file("./modules/argo/values/argo-webhook.yaml")
+  ]
+
+  wait = false
+
+  create_namespace = true
+
+  depends_on = [
+    helm_release.argo,
+    helm_release.argo-events,
+  ]
+}
+
 resource "helm_release" "argo-gatekeeper" {
   count = var.argo_count ? 1 : 0
 

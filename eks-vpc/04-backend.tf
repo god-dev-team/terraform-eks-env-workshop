1d1d74a212cbd19e3d49f668257527bf63449c36
Timur Galeev <timur.galeev@god.de>
2021-07-20 13:05:57 +0200

    Fix readme

--- eks-vpc/04-backend.tf
+++ eks-vpc/04-backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.14"
   backend "s3" {
     bucket         = "tfstate-demo-infra"
     key            = "terraform/states/eks.tfstate"
